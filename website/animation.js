5a0c7f7e92c7de2189c1b354c463d2c897c0d322
Cameron Bingham <cameron.bingham@vivint.com>
2019-04-28 19:59:18 -0600

    Commiting changes to preprocessing project for animations.

---
+++ website/animation.js
@@ -0,0 +1,71 @@
+function animateLogo() {
+    TweenMax.from("#mainLogo", 0.7, {x:-300, opacity:0, scale:0.5});
+}
+
+function hoverBounce1() {
+    TweenLite.to("#link1", 0.5, { ease: Bounce.easeOut, y: 5 });
+}
+
+function reset1() {
+    TweenLite.to("#link1", 0.5, { ease: Bounce.easeOut, y: 0 });
+}
+
+function hoverBounce2() {
+    TweenLite.to("#link2", 0.5, { ease: Bounce.easeOut, y: 5 });
+}
+
+function reset2() {
+    TweenLite.to("#link2", 0.5, { ease: Bounce.easeOut, y: 0 });
+}
+
+function hoverBounce3() {
+    TweenLite.to("#link3", 0.5, { ease: Bounce.easeOut, y: 5 });
+}
+
+function reset3() {
+    TweenLite.to("#link3", 0.5, { ease: Bounce.easeOut, y: 0 });
+}
+
+function hoverBounce4() {
+    TweenLite.to("#link4", 0.5, { ease: Bounce.easeOut, y: 5 });
+}
+
+function reset4() {
+    TweenLite.to("#link4", 0.5, { ease: Bounce.easeOut, y: 0 });
+}
+
+function hoverBounce5() {
+    TweenLite.to("#link5", 0.5, { ease: Bounce.easeOut, y: 5 });
+}
+
+function reset5() {
+    TweenLite.to("#link5", 0.5, { ease: Bounce.easeOut, y: 0 });
+}
+
+function hoverBounce6() {
+    TweenLite.to("#link6", 0.5, { ease: Bounce.easeOut, y: 5 });
+}
+
+function reset6() {
+    TweenLite.to("#link6", 0.5, { ease: Bounce.easeOut, y: 0 });
+}
+
+function logoRotate1() {
+    TweenMax.to("#logo1", 0.75, {rotationX:360});
+}
+
+function logoRotate2() {
+    TweenMax.to("#logo2", 0.75, {rotationX:360});
+}
+
+function logoRotate3() {
+    TweenMax.to("#logo3", 0.75, {rotationX:360});
+}
+
+function logoRotate4() {
+    TweenMax.to("#logo4", 0.75, {rotationX:360});
+}
+
+function logoRotate5() {
+    TweenMax.to("#logo5", 0.75, {rotationX:360});
+}
